d6d76aa378307e841958aee2750695478e5e0d45
baolinet <bao.li.net@gmail.com>
2018-02-24 10:36:24 +0800

    修改启动文件名

--- src/main/java/com/chanjetpay/garlic/GarlicBoltSiteApplication.java
+++ src/main/java/com/chanjetpay/garlic/GarlicBoltSiteApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GarlicBoltWebApplication {
+public class GarlicBoltSiteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GarlicBoltWebApplication.class, args);
+		SpringApplication.run(GarlicBoltSiteApplication.class, args);
 	}
 }
--- src/main/java/com/chanjetpay/garlic/ServletInitializer.java
+++ src/main/java/com/chanjetpay/garlic/ServletInitializer.java
@@ -1,6 +1,5 @@
 package com.chanjetpay.garlic;
 
-import com.chanjetpay.garlic.GarlicBoltWebApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
@@ -8,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(GarlicBoltWebApplication.class);
+		return application.sources(GarlicBoltSiteApplication.class);
 	}
 
 }
