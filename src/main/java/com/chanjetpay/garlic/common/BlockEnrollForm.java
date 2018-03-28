6ef62c6e6e32500469c307a8f12d820b6a6769e9
baolinet <bao.li.net@gmail.com>
2018-03-28 18:36:03 +0800

    完善核心服务中债权债务模块

--- src/main/java/com/chanjetpay/garlic/common/BlockEnrollForm.java
+++ src/main/java/com/chanjetpay/garlic/common/BlockEnrollForm.java
@@ -39,6 +39,6 @@ public class BlockEnrollForm {
 
 	@Override
 	public String toString() {
-		return ReflectionToStringBuilder.toString(this, ToStringStyle.MULTI_LINE_STYLE, true, true);
+		return ReflectionToStringBuilder.toString(this, ToStringStyle.DEFAULT_STYLE, true, true);
 	}
 }
